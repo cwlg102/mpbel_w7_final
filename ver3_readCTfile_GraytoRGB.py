827f191a6c59a2669a3882339f728f13e4c46e8c
larmor freq <100037095+cwlg102@users.noreply.github.com>
2022-07-08 18:14:00 +0900

    Add files via upload

--- ver3_readCTfile_GraytoRGB.py
+++ ver3_readCTfile_GraytoRGB.py
@@ -1,21 +1,23 @@
 import os
 import re
+from nbformat import write
 import numpy as np
 import pydicom
-from pydicom.pixel_data_handlers.util import apply_modality_lut, apply_voi_lut
+from pydicom.pixel_data_handlers.util import apply_modality_lut, apply_voi_lut, apply_windowing
 from numpngw import write_png
+import imageio
 #from PIL import Image #PIL은 8bit image만 다룰수 있음.
 #https://stackoverflow.com/questions/62739851/convert-rgb-arrays-to-pil-image
 
+basepath = 'D:\CodeMaster\w7_dataset'
+folder_path = basepath + '\P001_HN1_CT_2018-11-01_102652_RT^01.RT.Head.Neck.(Adult)_Head.&.Neck..3.0..B31s_n150__00000'
 
-folder_path = 'D:\CodeMaster\w7_dataset\P001_HN1_CT_2018-11-01_102652_RT^01.RT.Head.Neck.(Adult)_Head.&.Neck..3.0..B31s_n150__00000'
-zsize = int(folder_path[-10:-7])
 pic = []
 
 #CT 파일 불러오기
 #https://wikidocs.net/39 -> os.walk 사용법.
 
-CT_voxel = np.zeros((zsize, 512, 512, 3))
+
 filename_list = []
 fileidx = []
 for top, dir, file in os.walk(folder_path):
@@ -40,55 +42,79 @@ sorted_filename_list = []
 for i in range(len(fileidx)):
     sorted_filename_list.append(filename_list[fileorder[i]])
 
+zsize = len(sorted_filename_list)
+CT_voxel = np.zeros((zsize, 512, 512, 3), 'int32')
 #file정렬
-determine_company = pydicom.dcmread(folder_path + '/' + sorted_filename_list[0])
-window_center = -1000
-window_width = 6000
-if determine_company.Manufacturer == 'TOSHIBA':
-    window_center = -1200
-    window_width = 4000
+fordeterminepixelvalue = pydicom.dcmread(folder_path + '/' + sorted_filename_list[0])
+
+if fordeterminepixelvalue.PixelRepresentation == 1:
+    conv_coeff = 2**15
     for i in range(len(sorted_filename_list)):
-        ds = pydicom.dcmread(folder_path + '/' + sorted_filename_list[i])
-        pic.append(ds)
-        s = int(ds.RescaleSlope)
-        b = int(ds.RescaleIntercept)
-        temp_slice = s * ds.pixel_array + b#largepixelvalue로 나눠줌.
+        ct_dicomdata = pydicom.dcmread(folder_path + '/' + sorted_filename_list[i])
+        pic.append(ct_dicomdata)
+        slope = int(ct_dicomdata.RescaleSlope)
+        intercept = int(ct_dicomdata.RescaleIntercept)
+        temp_slice = slope * ct_dicomdata.pixel_array + intercept#largepixelvalue로 나눠줌.
+        temp_slice = temp_slice.astype('int32')
 
-        ds.WindowCenter = window_center
-        ds.WindowWidth = window_width
-        temp_slice = apply_modality_lut(temp_slice, ds)
-        temp_slice = apply_voi_lut(temp_slice, ds)
-
-        pixel_rgb = np.stack((temp_slice, ) * 3, axis = -1) #그레이 스케일 복셀 -> RGB로 바꾸어야...
-        CT_voxel[i] = pixel_rgb
-
-elif determine_company.Manufacturer == 'SIEMENS': #SIEMENS일때,,, 자체 windowing
-    for i in range(len(sorted_filename_list)):
-        ds = pydicom.dcmread(folder_path + '/' + sorted_filename_list[i])
-        pic.append(ds)
+        window_center = ct_dicomdata.WindowCenter
+        window_width = ct_dicomdata.WindowWidth
         
-        temp_slice = ds.pixel_array #largepixelvalue로 나눠줌.
-        pixel_rgb = np.stack((temp_slice, ) * 3, axis = -1) #그레이 스케일 복셀 -> RGB로 바꾸어야...
+        img_min = window_center - window_width // 2
+        img_max = window_center + window_width // 2
+
+        temp_slice[temp_slice < img_min] = img_min
+        temp_slice[temp_slice > img_max] = img_max
+
+        temp_slice *= round(conv_coeff/(img_max-img_min))
+        pixel_rgb = np.stack((temp_slice, ) * 3, axis = -1)
         CT_voxel[i] = pixel_rgb
-    CT_voxel /= 4095
-    CT_voxel *= 65535
-    CT_voxel = np.where(CT_voxel > 40000, 58000, CT_voxel)
-    CT_voxel = np.where(CT_voxel < 7000, 4000, CT_voxel)
-CT_voxel = CT_voxel.astype('uint16')
+
+#elif ispixelplus.Manufacturer == 'SIEMENS': #SIEMENS일때,,, 자체 windowing
+else: 
+    conv_coeff = 2**15
+    for i in range(len(sorted_filename_list)):
+        ct_dicomdata = pydicom.dcmread(folder_path + '/' + sorted_filename_list[i])
+        pic.append(ct_dicomdata)
+         #largepixelvalue로 나눠줌.
+        slope = int(ct_dicomdata.RescaleSlope)
+        intercept = int(ct_dicomdata.RescaleIntercept)
+        temp_slice = slope * ct_dicomdata.pixel_array + intercept
+        temp_slice = temp_slice.astype('int32')
+
+        window_center = ct_dicomdata.WindowCenter[1]
+        window_width = ct_dicomdata.WindowWidth[1]
+
+        img_min = window_center - window_width // 2
+        img_max = window_center + window_width // 2
+
+        temp_slice[temp_slice < img_min] = img_min
+        temp_slice[temp_slice > img_max] = img_max
+
+        temp_slice *= round(conv_coeff/(img_max-img_min))
+        pixel_rgb = np.stack((temp_slice, ) * 3, axis = -1)
+        CT_voxel[i] = pixel_rgb
+
+
 ctr_voxel = np.load('D:\CodeMaster\w7voxels\p001rtst\p001voxel_ctr.npy')
 
+print(np.min(CT_voxel))
+print(np.max(CT_voxel))
+CT_voxel += 32800-abs(np.min(CT_voxel))
+
+CT_voxel[CT_voxel <= np.min(CT_voxel)] = 0
+CT_voxel[CT_voxel >= np.max(CT_voxel)] = 2**16-1
+
+CT_voxel = CT_voxel.astype('uint16')
+
 ###################pls check manufaturer####################
-if ds.Manufacturer == 'TOSHIBA':
+if ct_dicomdata.Manufacturer != 'SIEMENS':
     ctr_voxel = np.flip(ctr_voxel, axis = 0)
 
 CT_ctr_voxel = np.where(ctr_voxel != [0, 0, 0], ctr_voxel, CT_voxel)
 np.save('D:\CodeMaster\w7voxels\p001ct\p001CTvoxel', CT_ctr_voxel)
 
+ 
 for k in range(len(CT_ctr_voxel)):
-    ####use PIL part######
-    #tempdata = CT_ctr_voxel[k]
-    #img = Image.fromarray(tempdata)
-    #img.save('D:\CodeMaster\w7voxels\p001ct\p001ct%d.png' %(k+1)) 
-    ####use numpngw part####
     write_png('D:\CodeMaster\w7voxels\p001ct\p001ct%d.png' %(k+1), CT_ctr_voxel[k])
     
